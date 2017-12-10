ff2b86326fe602664a30066b44d8f470a85dc6ea
peoxnen <sienski.witold@gmail.com>
2017-12-10 12:26:19 +0100

    add scopes

---
+++ app/src/main/java/iview/wsienski/mvvmtrip/di/scope/ActivityScope.java
@@ -0,0 +1,15 @@
+package iview.wsienski.mvvmtrip.di.scope;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Scope;
+
+/**
+ * Created by Witold Sienski on 10.12.2017.
+ */
+//TODO use scopes in project
+@Scope
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ActivityScope {
+}
---
+++ app/src/main/java/iview/wsienski/mvvmtrip/di/scope/FragmentScope.java
@@ -0,0 +1,15 @@
+package iview.wsienski.mvvmtrip.di.scope;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Scope;
+
+/**
+ * Created by Witold Sienski on 10.12.2017.
+ */
+
+@Scope
+@Retention(RetentionPolicy.RUNTIME)
+public @interface FragmentScope {
+}
